ad992f750516efdbd0ab618c963d615c1cab132c
LipzinXD <felipescamargo806@gmail.com>
2022-11-25 17:37:33 +0000

    includes

--- includes/header.php
+++ includes/header.php
@@ -16,10 +16,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/index.php">Gêneros</a>
+                    <a class="nav-link" aria-current="page" href="index.php">Gêneros</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="/list_livros.php">Livros</a>
+                <a class="nav-link" aria-current="page" href="livros_list.php">Livros</a>
                 </li>
             </ul>
 </div>
